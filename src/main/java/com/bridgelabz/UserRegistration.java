8a6a427a11211f8e896cc110fc34fe2337830d3a
shubhangi1208 <ashubhangi1996@gmail.com>
2022-02-01 12:23:33 +0530

    ADDED:Parameterized Test and Exception

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,60 +1,41 @@
 package com.bridgelabz;
 
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class UserRegistration {
-     Scanner scanner = new Scanner(System.in);
 
-    public boolean validFirstName(String name) {
-        String regex = "^[A-Z]{1}[a-z]{2,}$";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(name);
-        boolean r = m.matches();
-        if (r)
-            return true;
-        else
-            return false;
+    Scanner sc = new Scanner(System.in);
+
+    UserValidator userInformation = new UserValidator();
+
+    public String validateFirstName() throws UserRegistrationException {
+        System.out.println("Enter the First Name :- ");
+        String userFirstName = sc.nextLine();
+        return userInformation.validFirstName(userFirstName);
     }
-    public boolean validLastName(String lastname){
-        String regex = "^[A-Z]{1}[a-z]{3,}$";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(lastname);
-        boolean r = m.matches();
-        if (r)
-            return true;
-        else
-            return false;
+
+
+    public String validateLastName() throws UserRegistrationException {
+        System.out.println("Enter the Last Name :- ");
+        String userLastName = sc.nextLine();
+        return userInformation.validLastName(userLastName);
     }
-    public boolean validEmail(String emailID) {
-        String regex = "^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(emailID);
-        boolean r = m.matches();
-        if (r)
-            return true;
-        else
-            return false;
+
+    public String validateMailID() throws UserRegistrationException {
+        System.out.println("Enter the Mail ID :- ");
+        String userMailID = sc.nextLine();
+        return userInformation.validMailID(userMailID);
     }
-    public boolean validMobileNumber(String mobilenumber){
-        String regex3 = "^[9,1]{2}\\s[0-9]{10}$";
-        Pattern p = Pattern.compile(regex3);
-        Matcher m = p.matcher(mobilenumber);
-        boolean r = m.matches();
-        if (r)
-            return true;
-        else
-            return false;
+
+    public String validateMobileNumber() throws UserRegistrationException {
+        System.out.println("Enter the Mobile Number :- ");
+        String userMobileNumber = sc.nextLine();
+        return userInformation.validMobileNumber(userMobileNumber);
     }
-    public boolean validPassword(String password){
-        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()<>]).{8,}$";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(password);
-        boolean r = m.matches();
-        if (r)
-            return true;
-        else
-            return false;
+
+    public String validatePassword() throws UserRegistrationException {
+        System.out.println("Enter the Password :- ");
+        String userPassword = sc.nextLine();
+        return userInformation.validPassword(userPassword);
     }
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -4,35 +4,168 @@ import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class UserRegistrationTest extends UserRegistration {
+public class UserRegistrationTest {
+
+    String invalidResultExpected, invalidResultActual;
+
+    UserValidator validUserInfo = new UserValidator();
+
     @Test
-    public void givenFirstname_WhenProper_ShouldReturnFirstNameisValid() {
-        UserRegistration userRegistration= new UserRegistration();
-        boolean result = userRegistration.validFirstName("Shubhangi");
-        Assert.assertEquals(true,result);
+    public void givenFirstname_WhenProper_ShouldReturnUserEntryValidatedSuccessfully() throws UserRegistrationException {
+        try {
+            invalidResultExpected = "Happy:User Entry Validated Successfully";
+            invalidResultActual = validUserInfo.validFirstName("Shubhangi");
+            Assert.assertEquals(invalidResultExpected, invalidResultActual);
+        } catch ( UserRegistrationException e) {
+            Assert.assertEquals(UserRegistrationException.ExceptionType.INVALID_USER_FIRST_NAME, e.type);
+        }
     }
+
+
     @Test
-    public void givenLastname_WhenProper_ShouldReturnLastNameisValid() {
-        UserRegistration userRegistration= new UserRegistration();
-        boolean result = userRegistration.validLastName("Topale");
-        Assert.assertEquals(true,result);
+    public void givenFirstname_WhenIvalid_ShouldReturnUserEntryValidationFailed() throws UserRegistrationException {
+        try {
+            invalidResultExpected = "Sad:User Entry Validation Failed";
+            invalidResultActual = validUserInfo.validFirstName("shubhangi");
+            Assert.assertEquals(invalidResultExpected, invalidResultActual);
+        } catch ( UserRegistrationException e) {
+            Assert.assertEquals(UserRegistrationException.ExceptionType.INVALID_USER_FIRST_NAME, e.type);
+        }
     }
+
     @Test
-    public void givenEmail_WhenProper_ShouldReturnEmailisValid() {
-        UserRegistration userRegistration= new UserRegistration();
-        boolean result = userRegistration.validEmail("abc@yahoo.com");
-        Assert.assertEquals(true,result);
+    public void givenLastname_WhenProper_ShouldReturnUserEntryValidatedSuccessfully() throws  UserRegistrationException {
+        try {
+            invalidResultExpected = "Happy:User Entry Validated Successfully";
+            invalidResultActual = validUserInfo.validLastName("Amilkanthwar");
+            Assert.assertEquals(invalidResultExpected, invalidResultActual);
+        } catch ( UserRegistrationException e) {
+            Assert.assertEquals(UserRegistrationException.ExceptionType.INVALID_USER_FIRST_NAME, e.type);
+        }
     }
+
     @Test
-    public void givenMobileNo_WhenProper_ShouldReturnPhoneNoisValid() {
-        UserRegistration userRegistration= new UserRegistration();
-        boolean result = userRegistration.validMobileNumber("91 9096067476");
-        Assert.assertEquals(true,result);
+    public void givenLastname_WhenInvalid_ShouldReturnUserEntryValidationFailed() throws UserRegistrationException {
+        try {
+            invalidResultExpected = "Sad:User Entry Validation Failed";
+            invalidResultActual = validUserInfo.validLastName("amilkanthwar");
+            Assert.assertEquals(invalidResultExpected, invalidResultActual);
+        } catch ( UserRegistrationException e) {
+            Assert.assertEquals(UserRegistrationException.ExceptionType.INVALID_USER_FIRST_NAME, e.type);
+        }
     }
+
     @Test
-    public void givenPassword_WhenProper_ShouldReturnPasswordisValid() {
-        UserRegistration userRegistration =new UserRegistration();
-        boolean result= userRegistration.validPassword("Abc@1234");
-        Assert.assertEquals(true, result);
+    public void givenemailID_WhenValid_ShouldReturnUserEntryValidatedSuccessfully() throws  UserRegistrationException {
+        try {
+
+            invalidResultExpected = "Happy:User Entry Validated Successfully";
+            invalidResultActual = validUserInfo.validMailID("abc@yahoo.com");
+            String validResultActual1 = validUserInfo.validMailID("abc-100@yahoo.com");
+            String validResultActual2 = validUserInfo.validMailID("abc.100@yahoo.com");
+            String validResultActual3 = validUserInfo.validMailID("abc111@abc.com");
+            String validResultActual4 = validUserInfo.validMailID("abc-100@abc.net");
+            String validResultActual5 = validUserInfo.validMailID("abc.100@abc.com.au");
+            String validResultActual6 = validUserInfo.validMailID("abc@1.com");
+            String validResultActual7 = validUserInfo.validMailID("abc@gmail.com.com");
+            String validResultActual8 = validUserInfo.validMailID("abc+100@gmail.com");
+
+            Assert.assertEquals(invalidResultExpected, invalidResultActual);
+            Assert.assertEquals(invalidResultExpected, validResultActual1);
+            Assert.assertEquals(invalidResultExpected, validResultActual2);
+            Assert.assertEquals(invalidResultExpected, validResultActual3);
+            Assert.assertEquals(invalidResultExpected, validResultActual4);
+            Assert.assertEquals(invalidResultExpected, validResultActual5);
+            Assert.assertEquals(invalidResultExpected, validResultActual6);
+            Assert.assertEquals(invalidResultExpected, validResultActual7);
+            Assert.assertEquals(invalidResultExpected, validResultActual8);
+
+        } catch ( UserRegistrationException e) {
+            Assert.assertEquals(UserRegistrationException.ExceptionType.INVALID_USER_MAIL_ID, e.type);
+        }
+    }
+
+    @Test
+    public void givenemailID_WhenInvalid_ShouldReturnUserEntryValidationFailed()  {
+        try {
+            invalidResultExpected = "Sad:User Entry Validation Failed";
+            String invalidResultActual = validUserInfo.validMailID("abc");
+            String invalidResultActual1 = validUserInfo.validMailID("abc@.com.my");
+            String invalidResultActual2 = validUserInfo.validMailID("abc123@gmail.a");
+            String invalidResultActual3 = validUserInfo.validMailID("abc123@.com");
+            String invalidResultActual4 = validUserInfo.validMailID("abc123@.com.com");
+            String invalidResultActual5 = validUserInfo.validMailID(".abc@abc.com");
+            String invalidResultActual6 = validUserInfo.validMailID("abc()*@gmail.com");
+            String invalidResultActual7 = validUserInfo.validMailID("abc@%*.com");
+            String invalidResultActual8 = validUserInfo.validMailID("abc..2002@gmail.com");
+            String invalidResultActual9 = validUserInfo.validMailID("abc.@gmail.com");
+            String invalidResultActual10 = validUserInfo.validMailID("abc@abc@gmail.com");
+            String invalidResultActual11 = validUserInfo.validMailID("abc@gmail.com.1a");
+            String invalidResultActual12 = validUserInfo.validMailID("abc@gmail.com.aa.au");
+
+
+            Assert.assertEquals(invalidResultExpected, invalidResultActual);
+            Assert.assertEquals(invalidResultExpected, invalidResultActual1);
+            Assert.assertEquals(invalidResultExpected, invalidResultActual2);
+            Assert.assertEquals(invalidResultExpected, invalidResultActual3);
+            Assert.assertEquals(invalidResultExpected, invalidResultActual4);
+            Assert.assertEquals(invalidResultExpected, invalidResultActual5);
+            Assert.assertEquals(invalidResultExpected, invalidResultActual6);
+            Assert.assertEquals(invalidResultExpected, invalidResultActual7);
+            Assert.assertEquals(invalidResultExpected, invalidResultActual8);
+            Assert.assertEquals(invalidResultExpected, invalidResultActual9);
+            Assert.assertEquals(invalidResultExpected, invalidResultActual10);
+            Assert.assertEquals(invalidResultExpected, invalidResultActual11);
+            Assert.assertEquals(invalidResultExpected, invalidResultActual12);
+
+
+        } catch ( UserRegistrationException e) {
+            Assert.assertEquals(UserRegistrationException.ExceptionType.INVALID_USER_MAIL_ID, e.type);
+        }
+    }
+
+    @Test
+    public void givenMobileNum_WhenValid_ShouldReturnUserEntryValidatedSuccessfully() throws  UserRegistrationException {
+        try {
+            invalidResultExpected = "Happy:User Entry Validated Successfully";
+            invalidResultActual = validUserInfo.validMobileNumber("91-9096067476");
+            Assert.assertEquals(invalidResultExpected, invalidResultActual);
+        } catch ( UserRegistrationException e) {
+
+            Assert.assertEquals(UserRegistrationException.ExceptionType.INVALID_USER_MOBILE_NUMBER, e.type);
+        }
+    }
+
+    @Test
+    public void giveneMobileNum_WhenInvalid_ShouldReturnUserEntryValidationFailed() throws UserRegistrationException {
+        try {
+            invalidResultExpected = "Sad:User Entry Validation Failed";
+            invalidResultActual = validUserInfo.validMobileNumber("9096067476");
+            Assert.assertEquals(invalidResultExpected, invalidResultActual);
+        } catch ( UserRegistrationException e) {
+            Assert.assertEquals(UserRegistrationException.ExceptionType.INVALID_USER_MOBILE_NUMBER, e.type);
+        }
+    }
+
+    @Test
+    public void givenPassword_WhenValid_ShouldReturnUserEntryValidatedSuccessfully() throws UserRegistrationException {
+        try {
+            invalidResultExpected = "Happy:User Entry Validated Successfully";
+            invalidResultActual = validUserInfo.validPassword("Shubhangi@123");
+            Assert.assertEquals(invalidResultExpected, invalidResultActual);
+        } catch ( UserRegistrationException e) {
+            Assert.assertEquals(UserRegistrationException.ExceptionType.INVALID_USER_PASSWORD, e.type);
+        }
+    }
+
+    @Test
+    public void givenPassword_WhenInvalid_ShouldReturnUserEntryValidationFailed() throws  UserRegistrationException {
+        try {
+            invalidResultExpected = "Sad:User Entry Validation Failed";
+            invalidResultActual = validUserInfo.validPassword("shubhangi123");
+            Assert.assertEquals(invalidResultExpected, invalidResultActual);
+        } catch ( UserRegistrationException e) {
+            Assert.assertEquals(UserRegistrationException.ExceptionType.INVALID_USER_PASSWORD, e.type);
+        }
     }
 }
